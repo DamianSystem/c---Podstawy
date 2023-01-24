384a85fd763fcc345e59b41d52863f98d76963f0
Damian Korona <Damian.Korona@wp.pl>
2023-01-24 10:46:57 +0100

    update 06_case_kalkulator_responsywne sterowanie

--- 06_Case_Kalkulator/main.cpp
+++ 06_Case_Kalkulator/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
-
+#include <conio.h> // aby wykonaÄ‡ responsywne sterowanie tak jak w grach komuperowych
 
 using namespace std;
 
 float x,y;
-int wybor;
+char wybor; // zmiana typu danych na char #include <conio.h>
 
 int main()
 {
@@ -27,38 +27,40 @@ int main()
         cout<<"3. Mnozenie"<<endl;
         cout<<"4. Dzielenie"<<endl;
         cout<<"5. Koniec programu"<<endl;
-        cout <<"Wybierz: ";
-        cin >> wybor;
+
+        cout <<endl; //#include <conio.h>
+        wybor=getch(); //#include <conio.h>
+
         switch (wybor) // zmienna wybor
         {
-        case 1: //instrukcja pod numerem 1
+        case '1': //instrukcja pod numerem 1 zmiana na apostrofy  #include <conio.h>
 
         {
             cout<<"Suma = "<<x+y;
         }
         break;
 
-        case 2: //instrukcja pod numerem 2
+        case '2': //instrukcja pod numerem 2 zmiana na apostrofy #include <conio.h>
 
-        cout<<"Roznica = "<<x-y; //mo¿e byc bez klamer przy jednej instrukcji
+        cout<<"Roznica = "<<x-y; //moÂ¿e byc bez klamer przy jednej instrukcji
         break;
 
-        case 3: //instrukcja pod numerem 3
+        case '3': //instrukcja pod numerem 3 zmiana na apostrofy #include <conio.h>
 
         cout<<"Iloczyn = "<<x*y;
         break;
 
-        case 4: //instrukcja pod numerem 41
+        case '4': //instrukcja pod numerem 4 zmiana na apostrofy #include <conio.h>
 
             if (y=0) cout<<"Nie dzielimy przez zero!";
             else cout<<"Iloraz = "<<x/y;
         break;
 
-        case 5: //instrukcja pod numerem 41
+        case '5': //instrukcja pod numerem 5 zmiana na apostrofy #include <conio.h>
 
             exit(0);
         break;
-        default: //w pozosta³ych przypadkach
+        default: //w pozostaÂ³ych przypadkach
         cout<<"Nie ma takiej opcji w menu!";
         }
         getchar();
